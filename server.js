25d23f8065807acb5fb235186fa845739736fb37
Jake Thrasher <mjakethrasher@gmail.com>
2021-06-11 10:09:25 -0700

    add join room event listener

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { setUncaughtExceptionCaptureCallback } = require('process');
 const app = express();
 const PORT = process.env.PORT || 8000
+
 app.use(require('cors')({
   origin: true,
   credentials: true,
 }));
+
 const http = require('http').createServer(app)
 
 
@@ -17,23 +19,30 @@ const io = require('socket.io')(http, {
 
 app.use(express.json());
 
+
 io.on('connection', socket => {
   console.log(`new connection ${socket.id}`)
 
-  // socket.on('transmit mouse', (data) => {
-  //   console.log(data);
-  //   socket.broadcast.emit('mouse response', data)
+  socket.on('join room', (room) => {
+    console.log(socket)
 
-  // })
-
-  socket.on('ball dropped', data => {
-    console.log(new Date(), data)
-    io.emit('emit drop', data )
+    socket.join(room)
   })
+
+  socket.on('ball dropped', (room, data) => {
+    io.to(room).emit('emit drop', data )
+  }) 
+    
+  
   socket.on('ball move', data=> {
     io.emit('ball move', data)
   })
+
+  socket.on('leave', ()=>{
+    console.log(socket)
+  })
   
 })
+
 
 http.listen(PORT, () => console.log(`server spinning on port ${PORT}`));
